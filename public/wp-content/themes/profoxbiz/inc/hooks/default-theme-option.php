27f5228449ed5ba643cc08addf0a4e9f8838f608
Esteban Camargo <estebmaister@gmail.com>
2020-06-24 21:52:30 -0400

    :fire: removing extra files

--- public/wp-content/themes/profoxbiz/inc/hooks/default-theme-option.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( !function_exists('profoxbiz_default_theme_option')) :
-	function profoxbiz_default_theme_option( $options ){
-		$defaults = array(
-			'profoxbiz_blog_post_date_hide' => true,
-		);
-	}
-endif;
-add_action('profoxbiz_bailboard_slider_cat', 'profoxbiz_default_theme_option');
--- public/wp-content/themes/profoxbiz/inc/hooks/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include get_template_directory() . '/inc/hooks/default-theme-option.php';
-include get_template_directory() . '/inc/hooks/bailboard.php';
-include get_template_directory() . '/inc/hooks/services.php';
-include get_template_directory() . '/inc/hooks/portfolio.php';
-include get_template_directory() . '/inc/hooks/features.php';
-include get_template_directory() . '/inc/hooks/team.php';
-include get_template_directory() . '/inc/hooks/latest-post.php';
-include get_template_directory() . '/inc/hooks/testimonial.php';
